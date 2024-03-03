5bad3991a17bea10bc47a28e7b96c4d51f6ef3e3
luizfelipp <102230799+luizfelipp@users.noreply.github.com>
2024-03-03 07:17:11 -0300

    validate correção

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -67,7 +67,7 @@ class PostController extends Controller
             );
         }
         
-        $attrs = $request->validade(
+        $attrs = $request->validate(
             [
                 'body'=> 'required|string'
             ]
